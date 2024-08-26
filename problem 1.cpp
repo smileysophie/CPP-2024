9f9c5f9dfeea807b2c014c21b2ff8b485812f5ec
Sophie <90475851+smileysophie@users.noreply.github.com>
2024-08-26 11:12:35 -0500

    Create problem 1.cpp

---
+++ problem 1.cpp
@@ -0,0 +1,12 @@
+// Problem 1 - Solution
+
+#include <iostream> // library needed for input and output 
+using namespace std; // set default namespace to std
+
+int main() // starting point of your code
+{
+	// output
+	cout << "Hello World!"; // print the text 'Hello World!'
+
+	return 0; // return 0 to the caller
+}
