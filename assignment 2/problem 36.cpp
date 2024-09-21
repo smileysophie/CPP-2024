810afc72c961a31a4bdfd499f859dac5982c1706
Sophie <90475851+smileysophie@users.noreply.github.com>
2024-09-21 11:31:57 -0500

    Update problem 36.cpp

--- assignment 2/problem 36.cpp
+++ assignment 2/problem 36.cpp
@@ -1,5 +1,4 @@
 // Problem 36 - Solution
-// you need to finish this
 #include <iostream> // library needed for input and output
 using namespace std; // set default namespace to std
 
@@ -13,17 +12,23 @@ int main()
 
 
 	// input and process
-	for (int i = 0; i < 5; i++)
+	for (int i = 1; i < 6; i++)
 	{
 		// gets a number from the user as input
 		cout << "Please enter number " << i << ": ";
 		cin >> input;
 		// determines if it's the largest number
 		if (input > num1)
+		{
+			num2 = num1;
 			num1 = input;
+		}
 		else if (input > num2)
 			num2 = input;
 	}
+
+	// calculates the largest sum
+	sum = num1 + num2;
 
 	// outputs the two numbers which form the largest sum
 	cout << "Numbers " << num1 << " and " << num2 << " form the largest sum: " << sum;
