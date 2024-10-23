9a2a3782c31bf68909ceaea4c52e11ccd40724a0
Sophie <90475851+smileysophie@users.noreply.github.com>
2024-10-23 09:49:50 -0500

    Update problem 10.cpp

--- assignment 3/problem 10.cpp
+++ assignment 3/problem 10.cpp
@@ -1,5 +1,4 @@
 // Problem 10 - Solution
-// FINISH
 #include <iostream>
 using namespace std;
 
@@ -7,22 +6,28 @@ int main()
 {
 	// declare variables
 	const int SIZE = 3;
-	char ttt_board[SIZE][SIZE] = 
+	char ttt_board[SIZE][SIZE] =
 	{
-		{'X', 'O', 'X'},
-		{'O', 'O', 'X'},
-		{'X', 'X', 'X'}
+		{'O', 'X', 'X'},
+		{'X', 'X', ' '},
+		{'O' , 'O', 'O'}
 	};
 	char winner = 'N';
-
+	 
 	// process
-	// vertical check
-	if (ttt_board[0][0] == ttt_board[0][1] == ttt_board[0][2] || ttt_board[1][0] == ttt_board[1][1] == ttt_board[1][2] || ttt_board[2][0] == ttt_board[2][1] == ttt_board[2][2])
+	// determines if anyone has won, if statements are sorted by game tiles
+	if (ttt_board[0][0] == ttt_board[0][1] && ttt_board[0][1] == ttt_board[0][2] || ttt_board[0][0] == ttt_board[1][0] && ttt_board[1][0] == ttt_board[2][0] || ttt_board[0][0] == ttt_board[1][1] && ttt_board[1][1] == ttt_board[2][2])
 		winner = ttt_board[0][0];
-	// horizontal check
-	if (ttt_board[0][0] == ttt_board[1][0] == ttt_board[2][0] || ttt_board[0][1] == ttt_board[1][1] == ttt_board[2][1] || ttt_board[0][2] == ttt_board[1][2] == ttt_board[2][2])
-		winner = ttt_board[0][0];
-	// diagnol check
-	if (ttt_board[0][0] == ttt_board[1][1] == ttt_board[1][0] || ttt_board[0][1] == ttt_board[1][1] == ttt_board[2][1] || ttt_board[0][2] == ttt_board[1][2] == ttt_board[2][2])
+	else if (ttt_board[2][0] == ttt_board[1][1] && ttt_board[1][1] == ttt_board[0][2] || ttt_board[1][0] == ttt_board[1][1] && ttt_board[1][1] == ttt_board[1][2] || ttt_board[0][1] == ttt_board[1][1] && ttt_board[1][1] == ttt_board[2][1])
 		winner = ttt_board[1][1];
+	else if (ttt_board[2][0] == ttt_board[2][1] && ttt_board[2][1] == ttt_board[2][2] || ttt_board[0][2] == ttt_board[1][2] && ttt_board[1][2] == ttt_board[2][2])
+		winner = ttt_board[2][2];
+	else
+		winner = 'N';
+
+	// outputs the winner
+	if (winner == 'N')
+		cout << "Winner: None";
+	else
+		cout << "Winner: " << winner;
 }
