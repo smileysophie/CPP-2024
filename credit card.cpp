f5070ffe558c022e4685201ac4abede56c1dbb60
Sophie <90475851+smileysophie@users.noreply.github.com>
2024-11-04 22:36:28 -0600

    Update credit card.cpp

--- credit card.cpp
+++ credit card.cpp
@@ -1,35 +1,39 @@
 #include <iostream>
-#include "FileName.h"
 using namespace std;
 
 // function prototypes
-bool creditLuhn(int credit_num[])
+bool creditLuhn(int credit_num[16])
 {
 	// declare variables
 	bool valid = 0;
 	const int SIZE = 16;
 	int doubled = 0;
 	int total = 0;
-	
+
 	// process
 	// loops through the input array
-	for (int i = 1; i < SIZE; i+=2)
+	for (int i = SIZE - 2; i > -1; i -= 2)
 	{
 		// doubles every second digit
 		doubled = credit_num[i];
 		doubled *= 2;
-		cout << doubled;
 		// checks if doubling of number results in two digit number
 		if (doubled > 9)
 		{
 			// adds both digits together to form single digit
-			// sums all the digits
 			total += doubled / 10;
 			total += doubled % 10;
 
 		}
+		else
+			// adds the one digit number to the total
+			total += doubled;
 
 	}
+	// sums the rest of the digits
+	for (int i = SIZE - 1; i > -1; i -= 2)
+		total += credit_num[i];
+
 	// determines if the total modulo 10 is 0
 	if (total % 10 == 0)
 		// number is valid
@@ -46,6 +50,7 @@ int main()
 	const int CHAR_SIZE = 17;
 	char char_credit_num[CHAR_SIZE] = "";
 	int credit_num[CREDIT_SIZE] = {};
+	bool valid = 0;
 
 	// input
 	// accept a 16 digit credit card number as input
@@ -57,7 +62,14 @@ int main()
 
 	// process
 	// passes credit card number and size of array as argument to checkLuhn function
-	cout << creditLuhn(credit_num);
+	valid = creditLuhn(credit_num);
+
+	// output
+	// outputs whether the given credit card number is valid
+	if (valid == 1)
+		cout << "Credit card number is valid.";
+	else
+		cout << "Credit card number is invalid.";
 
 	return 0;
 }
