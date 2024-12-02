15adcfa8e5dad7fda8dae5d83f521b7555485b78
Sophie <90475851+smileysophie@users.noreply.github.com>
2024-12-02 10:53:10 -0600

    Update problem 3.cpp

--- assignment 5/problem 3.cpp
+++ assignment 5/problem 3.cpp
@@ -4,53 +4,33 @@
 #include <iostream>
 using namespace std;
 
-// function definition
-// finds subarray with a given sum
-void find_sub_recursive(int inp_arr[],  const int SIZE, int end, int target_sum, int current_sum, bool found, int start)
-{
-	
-	// if the index number is equal to the size of the array, stop the recursion
-	if (end == SIZE)
-		return;
 
-	// add the current element to the current sum
-	current_sum += inp_arr[end];
-	cout << current_sum << endl;
-
-	// if the current sum equals the target sum
-	if (current_sum == target_sum)
-	{
-		// outputs sum and where subarray is found
-		cout << "The target sum of " << target_sum << " can be found from start index " << start << " to end index " << end;
-		// sets the found flag to true
-		found = true;
-		// stops recursion
-		return;
-	}
-
-	// if the current sum is greater than the target sum
-	// try again
-	if (current_sum > target_sum)
-		find_sub_recursive(inp_arr, SIZE, end, target_sum, current_sum, found, start +1);
-	else
-		// moves on to the next element
-		find_sub_recursive(inp_arr, SIZE, end + 1, target_sum, current_sum, found, start);
-
-}
 
 // function definition
 // initiates recursion
-void find_sub(int inp_arr[], int target_sum, const int SIZE)
+bool find_sub(int inp_arr[], const int SIZE, int target_sum, int start, int& sub_start, int& sub_end, int current_sum = 0, int end = 0)
 {
-	// declares variable
-	bool found = 0;
-	
-	// calls find_sub_recursive to determine if substring was found
-	find_sub_recursive(inp_arr, SIZE, 0, target_sum, 0, found, 0);
+	// if done all, stop recursion
+	if (start == SIZE)
+		return false;
 
-	// if the subarray is not found
-	if (!found)
-		cout << "No subarray was found.";
+	// if the current sum matches the target sum
+	if (current_sum == target_sum)
+	{
+		sub_start = start;
+		sub_end = end - 1;
+		return true;
+	}
+	// if end index reaches past the target sum
+	if (end == SIZE || current_sum > target_sum)
+		// calls findsubarray to reset a new start index
+		return find_sub(inp_arr, SIZE, target_sum, start + 1, sub_start, sub_end, 0, start + 1);
+
+	// recursively calls itself
+	return find_sub(inp_arr, SIZE, target_sum, start, sub_start, sub_end, current_sum + inp_arr[end], end + 1);
+
+
+
 
 }
 int main()
@@ -59,9 +39,14 @@ int main()
 	const int SIZE = 10;
 	int inp_arr[SIZE] = { 9, 7, 2, 4, 25, 23, 2, 8, 10, 20 };
 	int target_sum = 10;
+	int sub_start = -1;
+	int sub_end = -1;
 
-	// calls the add_commission function to add commission to each value
-	find_sub(inp_arr, target_sum, SIZE);
+	// calls the find_sub function to output subarray
+	if (find_sub(inp_arr, SIZE, target_sum, 0, sub_start, sub_end))
+		cout << "The target sum of " << target_sum << " can be found from start index: " << sub_start << " to the end index: " << sub_end;
+	else
+		cout << "No subarray found.";
 
 	return 0;
 }
